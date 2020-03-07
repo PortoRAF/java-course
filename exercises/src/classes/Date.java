42805853cf6399ec2068845370d70458ab619ae7
Renato Porto <renatoafporto@gmail.com>
2020-03-06 21:56:20 -0300

    Now (with) this

--- exercises/src/classes/Date.java
+++ exercises/src/classes/Date.java
@@ -7,15 +7,22 @@ public class Date {
 	int year;
 	
 	Date() {
-		day = 1;
-		month = 1;
-		year = 1970;
+//		day = 1;
+//		month = 1;
+//		year = 1970;
+		this(1, 1, 1970);
 	}
 	
-	Date(int dayInit, int monthInit, int yearInit) {
-		day = dayInit;
-		month = monthInit;
-		year = yearInit;
+//	Date(int dayInit, int monthInit, int yearInit) {
+//		day = dayInit;
+//		month = monthInit;
+//		year = yearInit;
+//	}
+
+	Date(int day, int month, int year) {
+		this.day = day;
+		this.month = month;
+		this.year = year;
 	}
 	
 	String getFormattedDate() {
