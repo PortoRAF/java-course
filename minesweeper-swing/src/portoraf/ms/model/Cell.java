37200b0b161f3d8d32dd00275199d0a335a47e0f
rafpo <renatoafporto@gmail.com>
2020-03-23 16:23:00 -0300

    Imported console version of minesweeper into new project

---
+++ minesweeper-swing/src/portoraf/ms/model/Cell.java
@@ -0,0 +1,119 @@
+package portoraf.ms.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cell {
+
+	private final int row;
+	private final int col;
+	
+	private boolean mined = false;
+	private boolean opened = false;
+	private boolean flagged = false;
+	
+	private List<Cell> neighbors = new ArrayList<>();
+	
+	Cell(int row, int col) {
+		this.row = row;
+		this.col = col;
+	}
+	
+	boolean addNeighbor(Cell neighbor) {
+		boolean rowIsDiff = row != neighbor.row;
+		boolean colIsDiff = col != neighbor.col;
+		boolean diagonal = rowIsDiff && colIsDiff;
+		
+		int deltaRow = Math.abs(row - neighbor.row);
+		int deltaCol = Math.abs(col - neighbor.col);
+		int deltaAbsolute = deltaRow + deltaCol;
+		
+		if (deltaAbsolute == 1 && !diagonal) {
+			neighbors.add(neighbor);
+			return true;
+		}
+		else if (deltaAbsolute == 2 && diagonal) {
+			neighbors.add(neighbor);
+			return true;			
+		}
+		else {
+			return false;
+		}
+	}
+	
+	void toggleFlagged() {
+		if (!opened) {
+			flagged = !flagged;
+		}
+	}
+	
+	boolean open() {
+		if (!opened && !flagged) {
+			opened = true;			
+			if (mined) {
+				// TODO Implement new version
+			}			
+			if (safeNeighbor()) {
+				neighbors.forEach(n -> n.open());
+			}			
+			return true;
+		}
+		else {		
+			return false;
+		}
+	}
+	
+	boolean safeNeighbor() {
+		return neighbors.stream().noneMatch(n -> n.mined);
+	}
+	
+	void setMined() {
+		if (!mined) {
+			mined = true;
+		}
+	}
+	
+	public boolean isMined() {
+		return mined;
+	}
+	
+	public boolean isFlagged() {
+		return flagged;
+	}
+	
+	void setOpen() {
+		this.opened = true;
+	}
+	
+	public boolean isOpen() {
+		return opened;
+	}
+	
+	public boolean isClosed() {
+		return !isOpen();
+	}
+
+	public int getRow() {
+		return row;
+	}
+
+	public int getCol() {
+		return col;
+	}
+	
+	boolean goalMet() {
+		boolean disclosed = !mined && opened;
+		boolean secured = mined && flagged;
+		return disclosed || secured;
+	}
+	
+	long minesInVicinity() {
+		return neighbors.stream().filter(n -> n.mined).count();
+	}
+	
+	void restart() {
+       mined = false;     
+       opened = false;    
+       flagged = false;
+	}
+}
---
+++ minesweeper-swing/src/portoraf/ms/model/Field.java
@@ -0,0 +1,82 @@
+package portoraf.ms.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Field {
+
+	private int numOfRows;
+	private int numOfCols;
+	private int numOfMines;
+	
+	private final List<Cell> cells = new ArrayList<>();
+
+	public Field(int numOfRows, int numOfCols, int numOfMines) {		
+		this.numOfRows = numOfRows;
+		this.numOfCols = numOfCols;
+		this.numOfMines = numOfMines;
+		
+		generateCells();
+		bindNeighbors();
+		sortMines();
+	}
+	
+	public void open(int row, int col) {
+		try {
+			cells.parallelStream()
+				.filter(c -> c.getRow() == row && c.getCol() == col)
+				.findFirst()
+				.ifPresent(c -> c.open());
+		}
+		catch (Exception e) {
+			// FIXME Fix method 'open' implementation
+			cells.forEach(c -> c.setOpen());
+			throw e;
+		}
+	}
+	
+	public void flag(int row, int col) {
+		cells.parallelStream()
+		.filter(c -> c.getRow() == row && c.getCol() == col)
+		.findFirst()
+		.ifPresent(c -> c.toggleFlagged());
+	}
+
+	private void generateCells() {
+		for (int row = 0; row < numOfRows; row++) {
+			for (int col = 0; col < numOfCols; col++) {
+				cells.add(new Cell(row, col));
+			}
+		}		
+	}
+	
+	private void bindNeighbors() {
+		for (Cell c1: cells) {
+			for (Cell c2: cells) {
+				c1.addNeighbor(c2);
+			}
+		}		
+	}
+	
+	private void sortMines() {	
+		int index;
+		int mineCounter = 0;
+		do {
+			index = (int) (Math.random() * cells.size());
+			if (!cells.get(index).isMined()) {
+				cells.get(index).setMined();
+				mineCounter++;
+			}
+		}
+		while (mineCounter < numOfMines);
+	}	
+	
+	public boolean goalMet() {
+		return cells.stream().allMatch(c -> c.goalMet());
+	}
+	
+	public void restart() {
+		cells.stream().forEach(c -> c.restart());
+		sortMines();
+	}
+}
